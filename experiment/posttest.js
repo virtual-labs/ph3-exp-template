794c52745d7e957a11f3ee9270d45afe8612488d
Pavan <pavankumarpre@gmail.com>
2020-11-19 16:27:22 +0530

    Update posttest.js

--- experiment/posttest.js
+++ experiment/posttest.js
@@ -97,15 +97,16 @@
             correctAnswer: "a" ///// Write the correct option inside double quotes
         },
 
-        {
-            question: "2.This is a sample question:", ///// Write the question inside double quotes
-            answers: {
-                a: "This is a sample answer A", ///// Write the option 1 inside double quotes
-                b: "This is a sample answer B", ///// Write the option 2 inside double quotes
+    {
+      question: "<img src='images/8.PNG'><br>Identify the location of Secondary electron detector",  ///// Write the question inside double quotes
+      answers: {
+        a: "<img src='images/1b.png'>",                  ///// Write the option 1 inside double quotes
+        b: "<img src='images/1a.png'>",                  ///// Write the option 2 inside double quotes
+        c: "<img src='images/1c.PNG'>",      },
+      correctAnswer: "c"                ///// Write the correct option inside double quotes
+    },
 
-            },
-            correctAnswer: "a" ///// Write the correct option inside double quotes
-        }, 
+
         
     ];
 
